2278bf9c1d76f750805893719144084120cc91eb
Simon Stuart <lakraven@gmail.com>
2026-09-22 11:20:54 +0200

    Add ESP-IDF demo compile support; refs #1

---
+++ tests/support/esp_idf/nvs_flash.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <esp_err.h>
+
+inline esp_err_t nvs_flash_init() noexcept {
+    return ESP_OK;
+}
+
+
+inline esp_err_t nvs_flash_erase() noexcept {
+    return ESP_OK;
+}
