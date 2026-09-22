8184390979e3847b5330cb4afef0698ee711bada
Simon Stuart <lakraven@gmail.com>
2026-09-22 10:14:16 +0200

    Validate explicit filesystem binding profile; refs #1

--- tests/ContractCompile.cpp
+++ tests/ContractCompile.cpp
@@ -4,10 +4,20 @@ namespace {
 
     struct ContractBinding final {};
 
+    using ContractVfsProfile =
+            ESPressio::Persistence::EspIdf::VfsBindingProfile<
+                ESPressio::Persistence::RetentionLevel::Restart,
+                ESPressio::Persistence::TextCaseSensitivity::CaseSensitive,
+                ESPressio::Persistence::MediaRemovability::Fixed,
+                255U,
+                255U,
+                0x7FFFFFFFULL
+            >;
+
 
     static_assert([]() consteval {
         ESPressio::Persistence::ValidatePersistenceProvider<
-            ESPressio::Persistence::EspIdf::VfsFileStorage<ContractBinding>
+            ESPressio::Persistence::EspIdf::VfsFileStorage<ContractBinding, ContractVfsProfile>
         >();
         ESPressio::Persistence::ValidatePersistenceProvider<
             ESPressio::Persistence::EspIdf::NvsKeyValueStorage<ContractBinding>
